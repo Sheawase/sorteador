8944d75811f733a15f7fef16f3c36c59462d075c
Sheawase <jvsantosdejesus9@gmail.com>
2025-07-31 16:45:40 -0300

    aprendendo if e else

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function generatenumber() {
     const min = Math.ceil(document.querySelector('.input').value);
     const max = Math.ceil(document.querySelector('.input2').value);
 
+    if (min >= max) {
+        alert("o menor valor tem que ser MAIOR que o maior valor")
+    } else {
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result);
+    }
 }
